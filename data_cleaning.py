c52d3132faa75688277f3fcdad0931b08c42feda
prasiddhigyawali <65633555+prasiddhigyawali@users.noreply.github.com>
2022-07-14 11:32:31 -0700

    clean up

--- data_cleaning.py
+++ data_cleaning.py
@@ -30,6 +30,14 @@ def open_df(df):
 
 #===========================================================================================================================================
 
+def indexId(df):
+    df.insert(0,'indexID', np.arange(1,len(df)+1))
+    return df
+
+def diagnosticId(df):
+    df['diagnosticID'] = np.arange(1,len(df)+1)
+    return df
+
 def mst_add(dictionary,check,replace):
     '''
     adds values to MST dictionary
@@ -99,56 +107,56 @@ def sex(df):
 
 #===========================================================================================================================================
 
-def inConv(df):
+def inConv(df,col):
     """
     Converts length from inches to millimeters
     """
-    df['Length'] = df['Length'] * 25.4
+    df[col] = df[col] * 25.4
     return df
 
 #===========================================================================================================================================
 
-def lbsConv(df):
+def lbsConv(df,col):
     """
     Converts weight from pounds to grams
     """
-    df['Weight'] = df['Weight'] * 453.59237
+    df[col] = df[col] * 453.59237
     return df
 
 #===========================================================================================================================================
 
-def cmConv(df):
+def cmConv(df,col):
     """
     Converts length from cenitmeters to millimeters
     """
-    df['Length'] = df['Length'] * 10
+    df[col] = df[col] * 10
     return df
 
 #===========================================================================================================================================
 
-def kgConv(df):
+def kgConv(df,col):
     """
     Converts weight from kilograms to grams
     """
-    df['Weight'] = df['Weight'] * 1000
+    df[col] = df[col] * 1000
     return df
 
 #===========================================================================================================================================
 
-def mConv(df):
+def mConv(df,col):
     """
     Converts length from meters to millimeters
     """
-    df['Length'] = df['Length'] * 1000
+    df[col] = df[col] * 1000
     return df
 
 #===========================================================================================================================================
 
-def mgConv(df):
+def mgConv(df,col):
     """
     Converts weight from milligrams to grams
     """
-    df['Weight'] = df['Weight'] / 1000
+    df[col] = df[col] / 1000
     return df
 
 #===========================================================================================================================================
@@ -181,17 +189,17 @@ def colcheck(df):
         
 #have it break if the set difference isn't zero
 
-    return (f"These column names do not match the template: {error}\nThese required columns are missing: {missing_req}")
-
-#    # renames columns through user input
-#    col_names = []
-#    for i in range(len(df.columns)):
-#        inpt = input("What would you like column " + str(i + 1) + " to be named?: ")
-#        col_names.append(inpt)
-#    df.columns = col_names
-#    return df
+    return (f"These column names do not match the template: {error}\nThese required columns are missing: {missing_req}\nThis app will take care of the following columns: materialSampleID, eventID, measurementType, measurementValue, yearCollected")
 
 #===========================================================================================================================================
+
+def dynamicProperties(df):
+    geome_col_names = pd.read_csv("https://raw.githubusercontent.com/futres/fovt-data-mapping/master/Mapping%20Files/template_col_names.csv")
+    df_col_names = df.columns
+    error = list(set(df_col_names) - set(geome_col_names["Template Column Names"]))
+    if len(error) != 0:
+        df["dynamicProperties"] = df[error].apply(lambda x: x.to_json(), axis=1)
+    return df
 
 def countryValidity(df):
     '''
@@ -210,35 +218,29 @@ def add_ms_and_indivdID(df):
     """
     Adds unique hex value materialSampleID and eventID to dataframe
     """
-    df['materialSampleID'] = [uuid.uuid4().hex for _ in range(len(df.index))]
+    # These are the column names that could match materialSampleID if they all match
+    sample_cols = ["institutionCode", "individualID", "scientificName", "sex", 
+                "country", "stateProvince", "lifeStage", "verbatimAgeValue",
+                "basisOfRecord", "locality", "samplingProtocol", "yearCollected"]
+
+    # Dictionary of terms and bones created from ontology codebook
+    map_dict = dict(zip(aep_subset.term , aep_subset.bone))
+
+    # Creating temp_bone column to map the measurementType to the common bone name
+    long_data["temp_bone"] = long_data["measurementType"].map(map_dict)
+
+    # Creating a json column containing everything in sample_cols
+    long_data['temp_json'] = long_data[sample_cols].apply(lambda x: x.to_json(), axis=1)
+
+    # Grouping only if temp_bone and temp_json are the same, assigning numeric ID
+    long_data["materialSampleID"] = long_data.groupby(["temp_bone", "temp_json"]).ngroup()
+
+    # Dropping unnecessary columns
+    long_data = long_data.drop("temp_bone", axis = 1)
+    long_data = long_data.drop("temp_json", axis = 1)
+    # df['materialSampleID'] = [uuid.uuid4().hex for _ in range(len(df.index))]
     df = df.assign(individualID = np.arange(len(df)))
     return df
-
-#===========================================================================================================================================
-
-def handle_conversion(df):
-    """
-    Handles conversion based on user input
-    
-    """
-    # Applying weight converstion functions in accordance to inputed unit
-    if wght == "pounds":
-        df = lbsConv(df)
-    elif wght == "kilograms":
-        df = kgConv(df)
-    elif wght == " milligrams":
-        df = mgConv(df)
-
-    # Applying length converstion functions in accordance to inputed unit
-    if lngth == "inches":
-        df = inConv(df)
-    elif lngth == "centimeters":
-        df = cmConv(df)
-    elif lngth == " meters":
-        df = mConv(df)
-
-    return df
-
 #===========================================================================================================================================
 #TODO: dynamically update the id_vars with everything accept the term columns
 #How: Let the user give the column names or range id_vars
@@ -257,26 +259,21 @@ def dataMelt(df,arr):
     df = pd.melt(df, id_vars = ID_VARS, value_vars = dataCols, var_name = 'measurementType', value_name = 'measurementValue')
     return df
     
-#===========================================================================================================================================
+def measurementUnitsHelper(df):
+    if 'weight' in df['measurementType'].lower() or 'mass' in df['measurementType'].lower():
+        return 'g'
+    else:
+        return 'mm'
 
-#TODO: print out non geome columns and prompt user to proceed
-
-# def make_clickable(url, name):
-#     return '<a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(url,name)
-
-# def license(df,license):
+def measurementUnits(df):
+    if 'measurementUnits' in df.columns:
+        return df
     
-#     if(license == "BSD"):
-#         df['License'] = df.apply(lambda x: make_clickable(x['https://opensource.org/licenses/BSD-3-Clause'], x['License']), axis=1)
-#     elif(license == "CC-BY"):
-#         df['License'] = df.apply(lambda x: make_clickable(x['https://creativecommons.org/licenses/by/4.0/'], x['License']), axis=1)
-#     else:
-#         df['License'] = df.apply(lambda x: make_clickable(x['https://creativecommons.org/publicdomain/zero/1.0/legalcode'], x['License']), axis=1)
-#     df['name'] = df['name'].apply(lambda x: f'<a href="http://softhints.com/tutorial/{x}">{x}</a>')
-#     df.to_html(escape=False)
+    if 'measurementType' in df.columns:
+        df['measurementUnit'] = df.apply(lambda row: measurementUnitsHelper(row), axis=1) 
+    return df
 
-# def make_clickable(val):
-#     return '<a target="_blank" href="{}">{}</a>'.format(val, val)
+#===========================================================================================================================================
 
 def license(df,license):
     if(license == "BSD"):
@@ -287,96 +284,4 @@ def license(df,license):
         df = df.assign(License = "CC0")
     return df
 
-def to_json(df):
-    """
-    Turns all columns that are not matching to GEOME columns into a singular dynamicProperties column
-    """
-
-    geome_col_names = pd.read_csv("https://raw.githubusercontent.com/futres/fovt-data-mapping/master/Mapping%20Files/template_col_names.csv")
-    df_col_names = df.columns
-
-    non_geome_cols_names = list(set(df_col_names) - set(geome_col_names["Template Column Names"]))
-    non_geome_df_cols = df[non_geome_cols_names]
-
-    df = df.assign(dynamicProperties="")
-
-    for i in non_geome_df_cols.index:
-        df["dynamicProperties"][i] = non_geome_df_cols.loc[i].to_json()
-    
-    return df
-
 #===========================================================================================================================================
-
-def callAll(df):
-    """
-    Calls all standard data cleaning functions
-    """
-    print("Removing blank rows and columns...")
-    df = remove_rcna(df)
-    print("Adding verbatimLocality...")
-    df = verLocal(df)
-    #print("Cleaning materialSampleType...")
-    #df = matSampType(df)
-    print("Cleaning sex column...")
-    df = sex(df)
-    print("Cleaning yearCollected column...")
-    df = yc(df)
-    print("Adding materialSampleID and eventID...")
-    df = add_ms_and_evID(df)
-    print("Converting to long format...")
-    df = dataMelt(df)
-   # print("Generating dynamicProperties column...")
-   # df = to_json(df)
-    return df
-
-#===========================================================================================================================================
-
-'''
-if __name__ == '__main__':
-
-    #todo: we need to force the user to have correct column names before proceeding to cleaning
-
-    # import matSamp_dict
-    matSamp_dict = pd.read_csv("https://raw.githubusercontent.com/futres/fovt-data-mapping/master/Mapping%20Files/MST_dict.csv")
-
-    # import cougar data directly from github
-    df = pd.read_csv("https://raw.githubusercontent.com/futres/fovt-data-mapping/master/Scripts/pythonConversion/1987to2019_Cougar_Weight_Length_Public_Request.csv")
-
-    colcheck(df)
-
-    # asking what the units of the weight values are
-    wght = input("What units are the weight values in? ")
-
-    #todo: How do we handle mixed weight values here?
-
-    while True:
-        if wght == "pounds" or wght == "kilomgrams" or wght == "milligrams" or wght == "grams":
-            break
-        print("Sorry! That is not an accepted weight unit.")
-        print("This program accepts: ""pounds"", ""kilograms"", ""milligrams"", and ""grams"".")
-        wght = input("What units are the weight values in? ")
-
-    # asking what the units of the length values are
-    lngth = input("What units are the length values in? ")
-
-    while True:
-        if lngth == "inches" or lngth == "centimeters" or lngth == "meters" or lngth == "millimeters":
-            break
-        print("Sorry! That is not an accepted length unit.")
-        print("This program accepts: ""inches"", ""centimeters"", ""meters"", and ""millimeters"".")
-        lngth = input("What units are the length values in? ")
-
-    df = matSampType(df)
-    df = handle_conversion(df)
-    df = callAll(df)
-
-    # print data frame as it is right now
-    print("This is your current data frame: ")
-    print(df)
-
-    required_columns = ['eventID', 'country','locality','yearCollected','samplingProtocol',
-                        'materialSampleID', 'basisOfRecord','scientificName','diagnosticID',
-                        'measurementMethod','measurementUnit','measurementType','measurementValue']
-
-    print(f'These required columns are still missing: {list(set(required_columns) - set(df.columns))}')
-'''
